efa02019870c1103e6cff17f8b9a4fc7d03ef8d0
ahmed0011-c <61215330+ahmed0011-c@users.noreply.github.com>
2020-09-20 18:06:33 +1000

    Update batchClassify.m

--- project functions/batchClassify.m
+++ project functions/batchClassify.m
@@ -1,6 +1,26 @@
 function [outputArg1,outputArg2,outputArg3,outputArg4] = batchClassify(Job,params,allocate)
+
 %BATCHCLASSIFY Summary of this function goes here
 
+%first input is [Job]     1 to compute  0 to avoid
+%Job.classify =1
+% Job.predict=1
+
+%second input is [params] 
+%supplied with data so there is no need to change
+
+%third input is allocate
+%allocate.channel = n
+%allocate.operation =n 
+%allocate.datatype = 1 for training data and 2 for validation data
+
+% will generate output
+% outputArg1 = svm_c; %svm information e.g weights
+% outputArg2 = dirction; % data dirction
+% outputArg3= lol_threshold; %svm threshold
+% outputArg4=predict_condition'; % predict condition state 1 for awake and 2 for anesthesia
+% 
+% plot predict map 
 
 %% classify what file
 
@@ -9,13 +29,13 @@ function [outputArg1,outputArg2,outputArg3,outputArg4] = batchClassify(Job,param
 %ih= allocate.trial;
 
 if allocate.datatype ==1
-name1 =  sprintf('training_data.mat',i,ix);
+name1 =  sprintf('training_data.mat');
 end
 
 if allocate.datatype ==2
     
    
-name1 =  sprintf('validation_data.mat',i,ix);
+name1 =  sprintf('validation_data.mat');
 
 end
 
@@ -55,12 +75,18 @@ if Job.predict ==1
 
 end
 
+%plot validation prediciton
+imagesc(predict_condition) 
+colorbar
+title('valdiation data predict one for awake and zero for anesthesia')
+ylabel('trial number')
 
 
 %   Detailed explanation goes here
 outputArg1 = svm_c;
-outputArg2 = accuracy;
+outputArg2 = dirction;
 outputArg3= lol_threshold;
 outputArg4=predict_condition';
+
 end
 
